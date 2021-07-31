e1266128797a9a0f055ee0ec7c279b894489810c
Michael Kalamogan <michaelkalamogan@gmail.com>
2021-07-31 10:54:42 +0800

    chat

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
+
+const port = process.env.PORT || 3000;
+const INDEX = '/index.html'
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 const io = require('socket.io')(server);
-const port = process.env.PORT || 8080;
-
-app.use(express.static(path.join(__dirname, '../../build')));
-
-app.get('/', (req, res, next) => res.sendFile(__dirname + './index.html'));
 
 let users = []
 
